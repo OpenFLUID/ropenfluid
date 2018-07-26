578f211b81f779d48e1a58debad1bb5601453190
Jean-Christophe Fabre <jean-christophe.fabre@inra.fr>
2018-07-26 11:32:09 +0200

    Update of build process on Windows systems

--- build-win32.cmake
+++ build-win32.cmake
@@ -1,14 +1,14 @@
 
-# CMake script for building R OpenFLUID package (win32 binary) 
-# usage : cmake -P build-win32.cmake 
+# CMake script for building R OpenFLUID package (win32 binary)
+# usage : cmake -P build-win32.cmake
 
 # This script must be launched from MSYS console
 
-# Notice that environment variables must be correctly set 
-# in order to find the OpenFLUID framework, the R environnement 
+# Notice that environment variables must be correctly set
+# in order to find the OpenFLUID framework, the R environnement
 # and GCC tools (C++11)
 # See file win32-env.sh.tpl for more information
- 
+
 
 INCLUDE("config.in.cmake")
 
@@ -16,10 +16,11 @@ INCLUDE("cmake/display-infos.cmake")
 INCLUDE("cmake/preprocess.cmake")
 
 
-EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" 
+EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}"
                 "-E" "chdir" "${BUILDDIR}"
                 "R" "CMD" "build" "ROpenFLUID")
-                
-EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" 
+
+EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}"
                 "-E" "chdir" "${BUILDDIR}"
-                "R" "CMD" "INSTALL" "--build" "ROpenFLUID") 
+                "R" "CMD" "INSTALL" "--build" "--no-test-load"
+                                    "--library=${BUILDLIBRDIR}" "ROpenFLUID")
